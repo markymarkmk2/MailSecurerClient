444ebe31fe081380961e6e44137e3607b2279128
Mark Williams <mark@dimm.de>
2009-08-05 22:59:02 +0000

    Proxy

--- src/home/ServerConnect/StreamConnect.java
+++ src/home/ServerConnect/StreamConnect.java
@@ -45,20 +45,47 @@ public class StreamConnect extends Connect
 
         try
         {
-            File src = new File("z:\\tmp\\datasrc.tst" );
-            BufferedInputStream bis = new BufferedInputStream( new FileInputStream( src ) );
-            OutputStream os = sc.open_out_stream("z:\\tmp\\data.tst");
+            File src = new File("j:\\testdata.txt" );
 
-            byte[] buff = new byte[8192];
-
-            while (true)
+            int[] bc = {32*1024, 64*1024, 8192*1024};
+            for ( int i = 0; i < bc.length; i++ )
             {
-                int rlen = bis.read(buff);
-                if (rlen == -1)
-                    break;
-                os.write(buff, 0, rlen);
+                int bs = bc[i];
+
+
+                long start = System.currentTimeMillis();
+                long size = src.length();
+
+                BufferedInputStream bis = new BufferedInputStream( new FileInputStream( src ) );
+                OutputStream os = sc.open_out_stream("z:\\tmp\\data.tst");
+
+                byte[] buff = new byte[bs];
+
+                System.out.println("BS: " + bs + "Byte" );
+                int calls = 0;
+                long real_len = 0;
+                while (true)
+                {
+                    int rlen = bis.read(buff);
+                    if (rlen == -1)
+                        break;
+                    os.write(buff, 0, rlen);
+                    calls++;
+
+                    real_len += rlen;
+                    if (calls % 10 == 0)
+                    {
+                        long end = System.currentTimeMillis();
+                        float  ratio = (real_len *1.0f)/((end - start) *1024.0f);
+                        System.out.println(" Speed: "  + (calls *1000 )/(end - start) + " C/s, " + ratio + "MB/s");
+                    }
+                }
+                os.close();
+                bis.close();
+                long end = System.currentTimeMillis();
+                float  ratio = (size *1.0f)/((end - start) *1024.0f);
+                System.out.println("Overall: " + ratio + "MB/s");
             }
-            os.close();
         }
         catch (IOException iOException)
         {
